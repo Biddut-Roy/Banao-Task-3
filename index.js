8bbfb416c8465a47a5ae0cca3fbd153155ebe2bb
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-19 18:53:52 +0600

    previews all task data setup complete

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.use(bodyParser.json());
 
 const users = [];
 
+// Dummy data
+let posts = [
+  { id: 1, text: "Post 1", likes: 0, comments: [] },
+  { id: 2, text: "Post 2", likes: 0, comments: [] },
+];
+
 app.post("/register", (req, res) => {
   const { email, password, username } = req.body;
 
@@ -46,6 +52,70 @@ app.post("/forgotPassword", (req, res) => {
   }
 
   res.status(200).json({ message: "Password reset email sent" });
+});
+
+// task 2 code:
+
+// Get all posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+// Access a get data by ID
+app.get("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.json(post);
+});
+
+// Create a new post
+app.post("/posts", (req, res) => {
+  const { text } = req.body;
+  if (!text) {
+    return res.status(400).json({ message: "Text is required" });
+  }
+  const newPost = {
+    id: posts.length + 1,
+    text,
+    likes: 0,
+    comments: [],
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+// Like a post by ID
+app.post("/posts/:id/like", (req, res) => {
+  const postId = parseInt(req.params.id);
+  // console.log(postId);   check id
+  const post = posts.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  post.likes++;
+  res.json(post);
+});
+
+// Add a comment to a post by Id
+app.post("/posts/:id/comments", (req, res) => {
+  const postId = parseInt(req.params.id);
+  // console.log(postId);   check id
+  const post = posts.find((post) => post.id === postId);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  const { text } = req.body;
+  if (!text) {
+    return res.status(400).json({ message: "Text is required" });
+  }
+  const newComment = {
+    text,
+  };
+  post.comments.push(newComment);
+  res.status(201).json(post.comments);
 });
 
 // Start the server
