75ca22d21bcb7fd2c7242ea8105bfcb603b1a35a
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-20 12:03:32 +0600

    check all secure API check a verify token

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const users = [];
 const SECRET_KEY =
-  "fa6fdaa9c1b0c424051431aaf7d3ecb137d036f264ca70d9b3374d47b504daf6109ce0cc7fe6ae1a1948585b05df2584ac37e064fcf09096a1e33d421f2126a7"; //
+  "fa6fdaa9c1b0c424051431aaf7d3ecb137d036f264ca70d9b3374d47b504daf6109ce0cc7fe6ae1a1948585b05df2584ac37e064fcf09096a1e33d421f2126a7"; //change your secrete code
 
 // Dummy data
 let posts = [
-  { id: 1, text: "Post 1", likes: 0, comments: [] },
-  { id: 2, text: "Post 2", likes: 0, comments: [] },
+  { text: "Post 1", likes: 0, comments: [], email: "jack@gmail.com" },
+  { text: "Post 2", likes: 0, comments: [], email: "jack1@gmail.com" },
 ];
 
 // Middleware to authenticate JWT
-const authenticateJWT = (req, res, next) => {
+const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
 
   if (token) {
     jwt.verify(token, SECRET_KEY, (err, user) => {
@@ -69,7 +72,11 @@ app.post("/login", async (req, res) => {
     SECRET_KEY,
     { expiresIn: "1h" }
   );
-  res.cookie("token", token, { httpOnly: true });
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
   res.status(200).json({ message: "User successfully logged in" });
 });
 
@@ -86,19 +93,19 @@ app.post("/forgotPassword", (req, res) => {
 });
 
 // Example of a protected route
-app.get("/profile", authenticateJWT, (req, res) => {
+app.get("/profile", verifyToken, (req, res) => {
   res.json({ message: `Welcome ${req.user.username}`, user: req.user });
 });
 
 // task 2 code:
 
 // Get all posts
-app.get("/posts", (req, res) => {
+app.get("/posts", verifyToken, (req, res) => {
   res.json(posts);
 });
 
 // Access a get data by ID
-app.get("/posts/:id", (req, res) => {
+app.get("/posts/:id", verifyToken, (req, res) => {
   const postId = parseInt(req.params.id);
   const post = posts.find((post) => post.id === postId);
   if (!post) {
@@ -108,7 +115,7 @@ app.get("/posts/:id", (req, res) => {
 });
 
 // Create a new post
-app.post("/posts", (req, res) => {
+app.post("/posts", verifyToken, (req, res) => {
   const { text } = req.body;
   if (!text) {
     return res.status(400).json({ message: "Text is required" });
@@ -118,13 +125,14 @@ app.post("/posts", (req, res) => {
     text,
     likes: 0,
     comments: [],
+    email: req.user.email,
   };
   posts.push(newPost);
   res.status(201).json(newPost);
 });
 
 // Like a post by ID
-app.post("/posts/:id/like", (req, res) => {
+app.post("/posts/:id/like", verifyToken, (req, res) => {
   const postId = parseInt(req.params.id);
   // console.log(postId);   check id
   const post = posts.find((post) => post.id === postId);
@@ -136,7 +144,7 @@ app.post("/posts/:id/like", (req, res) => {
 });
 
 // Add a comment to a post by Id
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", verifyToken, (req, res) => {
   const postId = parseInt(req.params.id);
   // console.log(postId);   check id
   const post = posts.find((post) => post.id === postId);
